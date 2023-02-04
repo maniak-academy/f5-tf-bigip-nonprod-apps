c12c0028a9894f62c6d5765d547ee5f94bedf4df
SebbyCorp <sebastian@maniak.io>
2023-02-04 16:10:37 -0500

    updates

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  cloud {
+    organization = "sebbycorp"
+
+    workspaces {
+      name = "f5-tf-bigip-nonprod-apps"
+    }
+  }
+  required_providers {
+    bigip = {
+      source  = "F5Networks/bigip"
+      version = "1.16.2"
+    }
+  }
+}
+
+
+provider "vault" {
+  address = "http://192.168.86.169:8200"
+  token   = var.vault_token
+}
+
+provider "bigip" {
+  address  = var.bigipmgmt
+  username = var.bigipmgmtuser
+  password = var.bigippass
+}
+
+
