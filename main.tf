1ce660fad645702aa01f01922ee01c4eb75d6e9d
sebbycorp <sebastian.maniak@hashicorp.com>
2023-02-04 16:53:54 -0500

    updates

--- main.tf
+++ main.tf
@@ -11,13 +11,20 @@ terraform {
       source  = "F5Networks/bigip"
       version = "1.16.2"
     }
+   vault = {
+      source = "hashicorp/vault"
+      version = "3.12.0"
+    }
   }
 }
 
 
 provider "vault" {
-  address = "http://192.168.86.169:8200"
-  token   = var.vault_token
+  address = "https://vault-maniak-public-vault-41264194.d96179f0.z1.hashicorp.cloud:8200"
+  # token   = var.vault_token
+  username = var.vault_user
+  password = var.vault_pass
+  namespace = "admin/network"
 }
 
 provider "bigip" {
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "bigipmgmt" {
+  description = "bigipmgmt"
+  default     = ""
+}
+variable "bigipmgmtuser" {
+  description = "bigipmgmtuser"
+  default     = ""
+}
+variable "bigippass" {
+  description = "bigippass"
+  default     = ""
+}
+variable "vault_token" {
+  description = "vault token"
+  default     = ""
+}
+variable "vaultaddress" {
+  description = "vault address"
+  default     = ""
+}
+variable "vault_user" {
+  description = "vault user"
+}
+  
+variable "vault_pass" {
+  description = "vault pass"
+}
