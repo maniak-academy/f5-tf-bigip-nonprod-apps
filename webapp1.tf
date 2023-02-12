c8b2fb6b8054ed539cb10ab04f77803165dc7969
Sebastian Maniak <sebastian@maniak.io>
2023-02-12 15:44:49 -0500

    updates

--- webapp1.tf
+++ webapp1.tf
@@ -1,14 +1,15 @@
-module "livedemoapp1" {
+module "webapp1" {
   source              = "sebbycorp/app/bigip"
   version             = "1.0.9"
-  tenant              = "livedemoapp1"
-  common_name         = "livedemoapp1.maniak.academy"
+  tenant              = "webapp1"
+  common_name         = "webapp1.maniak.academy"
   as3tmpl             = "shttps"
   vault_pki_backend   = "pki_int"
-  vip_address         = "192.168.57.205"
+  vip_address         = "192.168.57.200"
   pki_name            = "maniak-dot-academy"
   pool_members_port   = "443"
   monitor             = "https"
   load_balancing_mode = "least-connections-member"
-  pool_members        = ["172.16.100.1", "172.16.100.5", "172.16.100.3"]
+  pool_members        = ["172.16.100.1", "172.16.100.2", "172.16.100.3"]
 }
+
