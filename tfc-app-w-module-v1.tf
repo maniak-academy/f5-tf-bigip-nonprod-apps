a934f77c92ea1d4933554654bd15464cacf81fde
Sebastian Maniak <sebastian@maniak.io>
2023-02-12 12:34:57 -0500

    adding new code

--- tfc-app-w-module-v1.tf
+++ tfc-app-w-module-v1.tf
@@ -13,3 +13,18 @@ module "tfc-app-w-module-v1" {
   pool_members        = ["172.16.100.1", "172.16.100.2", "172.16.100.3"]
 }
 
+
+module "demo-app1" {
+  source              = "sebbycorp/app/bigip"
+  version             = "1.0.9"
+  tenant              = "demo-app1"
+  common_name         = "demo-app1.maniak.academy"
+  as3tmpl             = "shttps"
+  vault_pki_backend   = "pki_int"
+  vip_address         = "192.168.57.202"
+  pki_name            = "maniak-dot-academy"
+  pool_members_port   = "443"
+  monitor             = "https"
+  load_balancing_mode = "least-connections-member"
+  pool_members        = ["172.16.100.4", "172.16.100.5"]
+}
