bde64188267a57fa5320253377f50775a20002c2
Sebastian Maniak <sebastian.maniak@hashicorp.com>
2023-02-12 21:39:14 -0500

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -27,19 +27,3 @@ module "backend" {
   load_balancing_mode = "least-connections-member"
   pool_members        = ["172.16.100.1", "172.16.100.2", "172.16.100.3"]
 }
-
-
-module "webapp2" {
-  source              = "sebbycorp/app/bigip"
-  version             = "1.0.9"
-  tenant              = "webapp2"
-  common_name         = "webapp2.maniak.academy"
-  as3tmpl             = "https"
-  vault_pki_backend   = "pki_int"
-  vip_address         = "192.168.57.210"
-  pki_name            = "maniak-dot-academy"
-  pool_members_port   = "80"
-  monitor             = "http"
-  load_balancing_mode = "least-connections-member"
-  pool_members        = ["172.16.100.3", "172.16.100.5"]
-}
