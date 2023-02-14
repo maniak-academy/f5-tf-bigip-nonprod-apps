2e624ece9b491baa7c729c8929e75114c38a5e02
Sebastian Maniak <sebastian.maniak@hashicorp.com>
2023-02-14 14:28:05 +0000

    updating format

--- external-lb-app.tf
+++ external-lb-app.tf
@@ -1,4 +1,4 @@
-module "external-lb" {
+module "external-lb-web" {
   source              = "sebbycorp/app/bigip"
   version             = "1.0.9"
   tenant              = "external"
@@ -10,5 +10,20 @@ module "external-lb" {
   pool_members_port   = "80"
   monitor             = "http"
   load_balancing_mode = "least-connections-member"
-  pool_members        = ["172.16.100.1", "172.16.100.2","172.16.100.3"]
+  pool_members        = ["172.16.100.1", "172.16.100.2", "172.16.100.3"]
+}
+
+module "external-lb-api" {
+  source              = "sebbycorp/app/bigip"
+  version             = "1.0.9"
+  tenant              = "external"
+  common_name         = "external-api.maniak.academy"
+  as3tmpl             = "https"
+  vault_pki_backend   = "pki_int"
+  vip_address         = "192.168.57.231"
+  pki_name            = "maniak-dot-academy"
+  pool_members_port   = "80"
+  monitor             = "http"
+  load_balancing_mode = "least-connections-member"
+  pool_members        = ["172.16.100.4", "172.16.100.5"]
 }
